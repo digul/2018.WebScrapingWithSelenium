50655c2ad483137e7ea9a67d4da754e76c3c8e9d
angularyuni <yoonheepark10@hotmail.com>
2018-04-26 17:53:47 -0400

    Add Login page test
    This test displays dashboard page when a site user types user id and password and clicks login button  on the site's login page

---
+++ src/main/java/com/coska/lab/selenium/lib/LoginPageDemo.java
@@ -0,0 +1,27 @@
+package com.coska.lab.selenium.lib;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPageDemo {
+
+	public static void main(String[] args) {
+
+		// System.setProperty("webdriver.chrome.driver", "C:/Coska/drivers/chromedriver/chromedriver.exe");
+		//	System.setProperty("webdriver.gecko.driver", "C:/Coska/drivers/geckodriver-v0.20.1-win64/geckodriver.exe");
+		
+		/* Test to login and submit input data */
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.surveymonkey.com/user/sign-in/");
+		
+		WebElement loginID = driver.findElement(By.id("username"));
+		loginID.sendKeys("*** Enter User ID Here ***");
+		WebElement loginPassword = driver.findElement(By.id("password"));
+		loginPassword.sendKeys("*** Enter Password Here***");
+		loginPassword.submit();
+
+	}
+
+}
