f8892208d3d574dc2d7939c4163933bf6fba5822
angularyuni <yoonheepark10@hotmail.com>
2018-04-27 13:35:53 -0400

    Update HomePageTest.java

--- src/test/java/com/coska/lab/selenium/monkey/HomePageTest.java
+++ src/test/java/com/coska/lab/selenium/monkey/HomePageTest.java
@@ -1,5 +1,32 @@
 package com.coska.lab.selenium.monkey;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class HomePageTest {
 
+  static WebDriver driver = new FirefoxDriver();
+
+	@Test
+	public void canGoToHomePage() {
+		HomePage homePage = new HomePage(driver);
+		homePage.goTo();
+		Assert.assertTrue(homePage.isAt());
+	}
+	
+	@Test
+	public void canGoToLoginPage() {
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.goTo();
+		loginPage.logIn();		
+		Assert.assertTrue(loginPage.isAt());
+	}
+	
+	@AfterClass
+	public static void cleanUp() {
+		driver.close();
+	} 
 }
